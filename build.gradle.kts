77bce8e76d1eab0bc6633394953e2eb518d750ad
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-12-18 13:00:03 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,14 @@ repositories {
     mavenCentral()
 }
 
+val javaSnapshotTestingVersion = "4.0.7"
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("io.github.origin-energy:java-snapshot-testing-junit5:4.0.7")
-    testImplementation("io.github.origin-energy:java-snapshot-testing-plugin-jackson:4.0.6")
+    testImplementation("io.github.origin-energy:java-snapshot-testing-junit5:$javaSnapshotTestingVersion")
+    testImplementation("io.github.origin-energy:java-snapshot-testing-plugin-jackson:$javaSnapshotTestingVersion")
     testImplementation("org.slf4j:slf4j-simple:2.0.9")
 
     testImplementation(platform("com.fasterxml.jackson:jackson-bom:2.16.0"))
