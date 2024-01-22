02e134f76c2df0f383136da32df7371fd8a4e29b
Ned Twigg <ned.twigg@diffplug.com>
2024-01-21 16:07:22 -0800

    Use selfie camera to convert to Json.

--- src/test/kotlin/org/rogervinas/MyImplTest.kt
+++ src/test/kotlin/org/rogervinas/MyImplTest.kt
@@ -1,6 +1,9 @@
 package org.rogervinas
 
-import com.diffplug.selfie.Selfie.expectSelfie
+import com.diffplug.selfie.Camera
+import com.diffplug.selfie.Selfie
+import com.diffplug.selfie.Snapshot
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -8,6 +11,7 @@ import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
 import kotlin.random.Random
+
 
 internal class MyImplTest {
     private val myImpl = MyImpl(
@@ -19,12 +23,21 @@ internal class MyImplTest {
     @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9])
     fun `should do something`(input: Int) {
         val myResult = myImpl.doSomething(input)
-        expectSelfie(myResult.toString()).toMatchDisk("$input")
+        expectSelfie(myResult).toMatchDisk("$input")
     }
 
     @Test
     fun `should do something more`() {
         val myResult = myImpl.doSomethingMore()
-        expectSelfie(myResult.toString()).toMatchDisk()
+        expectSelfie(myResult).toMatchDisk()
+    }
+
+    private fun expectSelfie(actual: MyResult): Selfie.DiskSelfie {
+        return Selfie.expectSelfie(actual, JSON_CAMERA)
+    }
+    private val JSON_CAMERA = Camera<Any> { actual ->
+        val mapper = ObjectMapper()
+        mapper.findAndRegisterModules()
+        Snapshot.of(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(actual))
     }
 }
