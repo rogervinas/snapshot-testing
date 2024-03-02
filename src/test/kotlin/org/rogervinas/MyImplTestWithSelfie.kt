b74b1ed4ed73d771ee6b6d292f401d8b57ee5a4d
Roger Viñas <roger.vinas@fastzink.com>
2024-03-02 15:20:53 +0900

    Add selfie tests

--- src/test/kotlin/org/rogervinas/MyImplTestWithSelfie.kt
+++ src/test/kotlin/org/rogervinas/MyImplTestWithSelfie.kt
@@ -1,9 +1,10 @@
 package org.rogervinas
 
-import au.com.origin.snapshots.Expect
-import au.com.origin.snapshots.junit5.SnapshotExtension
+import com.diffplug.selfie.Camera
+import com.diffplug.selfie.Selfie
+import com.diffplug.selfie.Snapshot
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.time.Clock
@@ -11,11 +12,8 @@ import java.time.Instant
 import java.time.ZoneId
 import kotlin.random.Random
 
-@ExtendWith(SnapshotExtension::class)
-internal class MyImplTest {
 
-    private lateinit var expect: Expect
-
+internal class MyImplTestWithSelfie {
     private val myImpl = MyImpl(
         Random(seed=1234),
         Clock.fixed(Instant.parse("2022-10-01T10:30:00.000Z"), ZoneId.of("UTC"))
@@ -25,12 +23,18 @@ internal class MyImplTest {
     @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9])
     fun `should do something`(input: Int) {
         val myResult = myImpl.doSomething(input)
-        expect.serializer("json").scenario("$input").toMatchSnapshot(myResult)
+        Selfie.expectSelfie(myResult, selfieCamera).toMatchDisk("$input")
     }
 
     @Test
     fun `should do something more`() {
         val myResult = myImpl.doSomethingMore()
-        expect.serializer("json").toMatchSnapshot(myResult)
+        Selfie.expectSelfie(myResult, selfieCamera).toMatchDisk()
+    }
+
+    private val selfieCamera = Camera<Any> { actual ->
+        val mapper = ObjectMapper()
+        mapper.findAndRegisterModules()
+        Snapshot.of(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(actual))
     }
 }
