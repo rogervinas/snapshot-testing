3675982d2f2f4347582d7a58bde5bbe85e0488ef
Roger Viñas <roger.vinas@fastzink.com>
2024-03-03 06:32:40 +0900

    Selfie parameterized with JSON

--- src/test/kotlin/org/rogervinas/MyImplTestWithSelfie.kt
+++ src/test/kotlin/org/rogervinas/MyImplTestWithSelfie.kt
@@ -12,7 +12,6 @@ import java.time.Instant
 import java.time.ZoneId
 import kotlin.random.Random
 
-
 internal class MyImplTestWithSelfie {
     private val myImpl = MyImpl(
         Random(seed = 1234),
@@ -23,7 +22,7 @@ internal class MyImplTestWithSelfie {
     @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9])
     fun `should do something`(input: Int) {
         val myResult = myImpl.doSomething(input)
-        Selfie.expectSelfie(myResult.toString()).toMatchDisk("$input")
+        Selfie.expectSelfie(myResult, selfieCamera).toMatchDisk("$input")
     }
 
     @Test
